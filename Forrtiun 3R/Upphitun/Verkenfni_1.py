3536cedadce92d3be4763c894df4b5c0fccf4a44
Palli <gudbrandsson74@gmail.com>
2016-01-14 15:51:05 +0000

    14 1 2016
    Skilaði for 2C meira í 3R

--- Forrtiun 3R/Upphitun/Verkenfni_1.py
+++ Forrtiun 3R/Upphitun/Verkenfni_1.py
@@ -1,8 +1,33 @@
+#verkefni 1
 sum = 0
 for x in range(0, 1001):
-    if x % 3 == 0:
-    	sum += 3 
+	if x % 3 == 0:
+		sum = sum + 3
 	elif x % 5 == 0:
-		sum += 5
+		sum = sum + 5
 print sum
+
+#verkefni 2
+sum = 1
+print str(sum + ", ")
+last1 = 2
+print str(last1 + ", ")
+last2 = 3
+print str(last2 + ", ")
+
+while sum != 4000000:
+	sum = last2 +last1
+	print str(sum + ", ")
+	last1 = last2
+	last2 = sum
+
+#verkefni 3
+'''
+lausn = 0
+for x in range(1,600851475143):
+	if x % 600851475143 == 0:
+		lausn = x
+print lausn
+'''
+
 
