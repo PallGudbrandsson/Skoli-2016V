eeed5c2039524d35b33c61d8a2effc54bbbe7f12
PallGudbrandsson <gudbrandsson74@gmail.com>
2016-04-29 20:21:32 +0000

    Setja inn sleeptimes

---
+++ Forritun_2C/Lokaverkefni/SleepTimes/SleepTimes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace SleepTimes
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Random rand = new Random ();
+
+			int activeNum = rand.Next (1, 100);
+			int userNum = rand.Next (activeNum+5, activeNum + 15);
+
+			while (activeNum != userNum) {
+				Console.Clear ();
+				Console.WriteLine ("Active Number: " + activeNum);
+				Console.WriteLine ("Your number: " + userNum);
+
+				int sleeptime = rand.Next (100, 6000);
+				System.Threading.Thread.Sleep (sleeptime);
+				activeNum++;
+			}
+			Console.WriteLine ("it's your turn");
+		}
+	}
+}
