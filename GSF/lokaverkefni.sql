aed99932865313a6462831902fb344f26518c0a4
PallGudbrandsson <gudbrandsson74@gmail.com>
2016-05-09 17:14:22 +0000

    Vonandi virkar allt

--- GSF/lokaverkefni.sql
+++ GSF/lokaverkefni.sql
@@ -42,7 +42,7 @@ end $$
 DROP PROCEDURE IF EXISTS deleteStudentCource $$
 CREATE PROCEDURE deleteStudentCource(sid int, gid int)
 begin
-DELETE 
+DELETE FROM studentregistration WHERE studentID = sid AND groupID = gid
 end $$
 
 
@@ -50,25 +50,41 @@ DROP FUNCTION IF EXISTS numberOfStudents $$
 CREATE FUNCTION numberOfStudents()
 returns int
 begin
-
+RETURN(SELECT COUNT(studentID)) FROM students WHERE graduationYear > NOW());
 end $$
 
 
 DROP FUNCTION IF EXISTS totalNumberOfStudentCredits $$
-CREATE FUNCTION totalNumberOfStudentCredits()
+CREATE FUNCTION totalNumberOfStudentCredits(sid int)
 returns int
 begin
-
+RETURN(
+	SELECT COUNT(courses.courseCredits) FROM courses
+	JOIN groups ON courses.courseNumber = groups.courseNumber
+	JOIN studentregistration ON groups.groupID = studentregistration.groupID
+	WHERE studentregistration.studentID = sid
+	);
 end $$
 
 
 DROP FUNCTION IF EXISTS totalNumberOfStudentTakingSpecificCourse
-CREATE FUNCTION totalNumberOfStudentTakingSpecificCourse()
+CREATE FUNCTION totalNumberOfStudentTakingSpecificCourse(cnum char)
 returns int
 begin
-
+RETURN(
+	SELECT COUNT(studentregistration.regisrationNumber) FROM studentregistration
+	JOIN groups ON studentregistration.groupID = groups.groupID
+	JOIN courses ON groups.courseNumber = courses.courseNumber
+	WHERE courses.courseNumber = cnum
+);
 end $$
 
-
+DROP VIEW IF EXISTS avoidSiggi $$
+CREATE VIEW avoidSiggi
+SELECT * FROM courses
+WHERE courseNumber IS NOT "GSF2A3U" AND courseNumber IS NOT "GSF2B3U" AND courseNumber IS NOT "GSF3A3U" AND courseNumber IS NOT "FOR2G3U" AND courseNumber IS NOT "FOR4A3U"
 --view afangar sem siggi er ekki ad kenna
 --view allir forritunar afangar
+DROP VIEW IF EXISTS allProgrammingCourses $$
+CREATE VIEW allProgrammingCourses
+SELECT * FROM courses WHERE courseNumber LIKE "FOR%"
